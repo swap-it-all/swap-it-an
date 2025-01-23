9f80d1e57c380e98a078644cc1c213f0bf610148
kkosang <eiwoqp77@gmail.com>
2025-01-23 22:54:46 +0900

    feat: Padding 추가

--- app/src/main/java/com/example/swap_it/ui/component/Buttons.kt
+++ app/src/main/java/com/example/swap_it/ui/component/Buttons.kt
@@ -1,6 +1,7 @@
 package com.example.swap_it.ui.component
 
 import androidx.compose.foundation.interaction.InteractionSource
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.PaddingValues
 import com.example.swap_it.ui.theme.Shapes
@@ -9,10 +10,15 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import com.example.swap_it.ui.theme.Gray5
+import com.example.swap_it.ui.theme.Paddings
 import com.example.swap_it.ui.theme.Primary
 import com.example.swap_it.ui.theme.PrimaryDark
 import com.example.swap_it.ui.theme.Typography
@@ -24,7 +30,10 @@ fun DefaultButton(
     modifier: Modifier = Modifier,
     enabled: Boolean = false,
     contentStyle: TextStyle = Typography.titleMedium,
-    contentPadding: PaddingValues = PaddingValues(horizontal = 32.dp, vertical = 16.dp),
+    contentPadding: PaddingValues = PaddingValues(
+        horizontal = Paddings.xextra,
+        vertical = Paddings.xlarge,
+    ),
     interactionSource: InteractionSource,
     onClick: () -> Unit
 ) {
@@ -49,7 +58,6 @@ fun DefaultButton(
     }
 }
 
-/*
 class EnabledPreviewParameterProvider : PreviewParameterProvider<Boolean> {
     override val values = sequenceOf(
         true,
@@ -70,4 +78,3 @@ fun DefaultButtonPreview(
         interactionSource = remember { MutableInteractionSource() }
     )
 }
-*/
