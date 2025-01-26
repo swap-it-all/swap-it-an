98e09d7115ad26ab16c772bf9302030b44699e5f
kkosang <eiwoqp77@gmail.com>
2025-01-26 15:53:16 +0900

    feat: 타이포그래피 자간 수정

--- app/src/main/java/com/example/swap_it/ui/theme/Type.kt
+++ app/src/main/java/com/example/swap_it/ui/theme/Type.kt
@@ -19,84 +19,84 @@ val Typography = Typography(
         fontFamily = pretendard,
         fontSize = 36.sp,
         fontWeight = FontWeight.Bold,
-        letterSpacing = (-2).sp,
+        letterSpacing = (-0.25).sp,
         lineHeight = 54.sp
     ),
     headlineMedium = TextStyle(
         fontFamily = pretendard,
         fontSize = 24.sp,
         fontWeight = FontWeight.Bold,
-        letterSpacing = (-2).sp,
+        letterSpacing = (-0.25).sp,
         lineHeight = 36.sp
     ),
     headlineSmall = TextStyle(
         fontFamily = pretendard,
         fontSize = 21.sp,
         fontWeight = FontWeight.Bold,
-        letterSpacing = (-2).sp,
+        letterSpacing = (-0.25).sp,
         lineHeight = 31.5.sp
     ),
     titleLarge = TextStyle(
         fontFamily = pretendard,
         fontSize = 18.sp,
         fontWeight = FontWeight.Bold,
-        letterSpacing = (-2).sp,
+        letterSpacing = (-0.25).sp,
         lineHeight = 26.1.sp
     ),
     titleMedium = TextStyle(
         fontFamily = pretendard,
         fontSize = 16.sp,
         fontWeight = FontWeight.Bold,
-        letterSpacing = (-2).sp,
+        letterSpacing = (-0.25).sp,
         lineHeight = 24.sp
     ),
     titleSmall = TextStyle(
         fontFamily = pretendard,
         fontSize = 14.sp,
         fontWeight = FontWeight.Bold,
-        letterSpacing = (-2).sp,
+        letterSpacing = (-0.25).sp,
         lineHeight = 18.2.sp
     ),
     bodyLarge = TextStyle(
         fontFamily = pretendard,
         fontSize = 18.sp,
         fontWeight = FontWeight.Normal,
-        letterSpacing = (-2).sp,
+        letterSpacing = (-0.25).sp,
         lineHeight = 26.1.sp
     ),
     bodyMedium = TextStyle(
         fontFamily = pretendard,
         fontSize = 16.sp,
         fontWeight = FontWeight.Normal,
-        letterSpacing = (-2).sp,
+        letterSpacing = (-0.25).sp,
         lineHeight = 22.4.sp
     ),
     bodySmall = TextStyle(
         fontFamily = pretendard,
         fontSize = 14.sp,
         fontWeight = FontWeight.Normal,
-        letterSpacing = (-2).sp,
+        letterSpacing = (-0.25).sp,
         lineHeight = 18.2.sp
     ),
     labelLarge = TextStyle(
         fontFamily = pretendard,
         fontSize = 12.sp,
         fontWeight = FontWeight.Normal,
-        letterSpacing = (-2).sp,
+        letterSpacing = (-0.25).sp,
         lineHeight = 15.6.sp
     ),
     labelMedium = TextStyle(
         fontFamily = pretendard,
         fontSize = 11.sp,
         fontWeight = FontWeight.Light,
-        letterSpacing = (-2).sp,
+        letterSpacing = (-0.25).sp,
         lineHeight = 15.95.sp
     ),
     labelSmall = TextStyle(
         fontFamily = pretendard,
         fontSize = 10.sp,
         fontWeight = FontWeight.Light,
-        letterSpacing = (-2).sp,
+        letterSpacing = (-0.25).sp,
         lineHeight = 14.sp
     ),
 )
