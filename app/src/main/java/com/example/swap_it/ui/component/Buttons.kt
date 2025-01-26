a123174c560ffb2ffebff51aef7c68c8eccce143
kkosang <eiwoqp77@gmail.com>
2025-01-26 15:50:37 +0900

    feat: swap_it modalButton 추가

--- app/src/main/java/com/example/swap_it/ui/component/Buttons.kt
+++ app/src/main/java/com/example/swap_it/ui/component/Buttons.kt
@@ -1,10 +1,10 @@
 package com.example.swap_it.ui.component
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.interaction.InteractionSource
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.PaddingValues
-import com.example.swap_it.ui.theme.Shapes
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
@@ -12,14 +12,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import com.example.swap_it.ui.theme.Gray3
+import com.example.swap_it.ui.theme.Gray4
 import com.example.swap_it.ui.theme.Gray5
 import com.example.swap_it.ui.theme.Paddings
 import com.example.swap_it.ui.theme.Primary
 import com.example.swap_it.ui.theme.PrimaryDark
+import com.example.swap_it.ui.theme.Shapes
 import com.example.swap_it.ui.theme.Typography
 import com.example.swap_it.ui.theme.White
 
@@ -48,7 +52,53 @@ fun DefaultButton(
             contentColor = White,
             disabledContainerColor = Gray5,
         ),
-        shape = Shapes.large,
+        shape = Shapes.small,
+    ) {
+        Text(
+            text = text,
+            style = contentStyle,
+        )
+    }
+}
+
+@Composable
+fun ModalButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    contentStyle: TextStyle = Typography.titleMedium,
+    contentPadding: PaddingValues = PaddingValues(
+        horizontal = Paddings.extra,
+        vertical = Paddings.large,
+    ),
+    containerColor: Color = Primary,
+    contentColor: Color = White,
+    onClick: () -> Unit
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+    val animatedContainerColor by animateColorAsState(
+        targetValue = when (containerColor) {
+            Primary -> if (isPressed) PrimaryDark else containerColor
+            else -> if (isPressed) Gray4 else containerColor
+        }
+    )
+    val animatedContentColor by animateColorAsState(
+        targetValue = when (animatedContainerColor) {
+            Primary, PrimaryDark -> White
+            else -> Gray3
+        },
+    )
+
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        contentPadding = contentPadding,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = animatedContainerColor,
+            contentColor = animatedContentColor
+        ),
+        interactionSource = interactionSource,
+        shape = Shapes.small,
     ) {
         Text(
             text = text,
@@ -75,5 +125,24 @@ fun DefaultButtonPreview(
         contentStyle = Typography.titleMedium,
         enabled = enabled,
         interactionSource = remember { MutableInteractionSource() }
+    )
+}
+
+class ModalButtonPreviewParameterProvider : PreviewParameterProvider<Color> {
+    override val values = sequenceOf(
+        Primary,
+        Gray5,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ModalButtonPreview(
+    @PreviewParameter(ModalButtonPreviewParameterProvider::class) containerColor: Color
+) {
+    ModalButton(
+        text = "모달 버튼",
+        onClick = {},
+        containerColor = containerColor,
     )
 }
