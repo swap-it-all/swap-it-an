377831269387fb63cb2d4a99e638a090845e395c
Fluorite <102038962+FLU0RITE@users.noreply.github.com>
2025-02-06 17:01:31 +0900

    [UI] 프로덕트 리스트 스크린 및 메뉴 스크린 작성 (#7)
    * feat: 프로덕트 리스트 스크린 추가
    
    * feat: 로고 파일 추가 및 적용
    
    * feat: 바텀 바 추가
    
    * refactor: 바텀 바 버튼 선언 위치 변경
    
    * config: 이미지 로딩 실패로 인한 코일 라이브러리 추가
    
    * feat: Coil 라이브러리 적용 및 메뉴 스크린 UI 개선
    
    - Coil Compose 라이브러리를 프로젝트에 추가하여 이미지 로딩 기능을 개선했습니다.
    - `MenuScreen`의 UI를 개선하여, 상품 목록을 더 효율적으로 표시하도록 변경했습니다.
    - 바텀 앱 바의 배경색을 흰색으로 설정했습니다.
    - `MenuScreen`내부에서 상품 리스트가 보이는 화면의 top padding 값을 조정했습니다.
    - 불필요한 Theme, Surface 코드를 제거했습니다.
    
    * feat: 프로덕트 리스트 스크린 및 리스트 카드 추가
    
    * feat: 상품 리스트 카드 컴포넌트 추가
    * feat: 상품 리스트 스크린 추가
    
    * feat: Jetpack Compose Navigation 의존성 추가
    
    *   `build.gradle.kts`: Jetpack Compose Navigation 라이브러리 의존성 추가
    *   `libs.versions.toml`: Jetpack Compose Navigation 라이브러리 버전 추가
    
    * feat: 바텀 앱 바 적용을 위한 머터리얼 라이브러리 추가
    
    * feat: 바텀 네비게이션 바 추가
    
    * feat: BottomBar.kt 파일 생성 및 바텀 네비게이션 바 구현
    * feat: 바텀 네비게이션 바 아이템 상수 추가
    * feat: MenuScreen 바텀 네비게이션 바 적용
    * feat: BottomBar 스크린 추가
    
    * feat: 하단 네비게이션 바 스크린 추가
    
    *   feat: 물건 등록 스크린 추가
    *   feat: 채팅 스크린 추가
    *   feat: 거래 스크린 추가
    *   feat: 유저 정보 스크린 추가
    * refactor: 하단 네비게이션 바 각 화면들을 클래스화 하여 분리
    
    * refactor: ListCard 컴포넌트 패딩 값 변경
    
    - ListCard 컴포넌트의 패딩 값을 하드코딩 된 dp 값에서 디자인 시스템의 Paddings 값으로 변경했습니다.
    
    * refactor: 메뉴 스크린을 RootScreen으로 이름 변경 및 홈 -> 상품으로 네비게이션 명칭 변경
    
    * refactor: MenuScreen.kt 파일을 RootScreen.kt 파일로 이름 변경
    * refactor: BottomBar 네비게이션 아이템에서 홈(HOME) -> 상품(PRODUCT)으로 명칭 및 경로 변경
    * refactor: Const.kt 에서 HOME 상수를 PRODUCT 로 변경
    * refactor: MainActivity.kt 에서 MenuScreen -> RootScreen으로 변경
    
    * refactor: ProductCards 및 ProductListScreen 리팩터링
    
    *   refactor: `Cards.kt`를 `ProductCards.kt`로 파일 이름 변경
    *   refactor: `ListCard` 컴포저블을 `ProductListCard`로 이름 변경
    *   refactor: `ProductListScreen`의 `LazyColumn` 내 불필요한 주석 및 공백 제거
    *   refactor: `ProductListScreen` 에서 `ListCard` 호출 부분을 `ProductListCard` 로 변경
    
    * refactor: 기본 화면 이름 변경 및 바텀 바 선언 위치 변경
    
    * feat: 상품 리스트 스크린 필터 및 정렬 기능 추가
    
    * feat: 필터 버튼 및 정렬 모달 바텀 시트 추가
    * feat: 카테고리 버튼 및 카테고리 리스트 추가
    * feat: 정렬 버튼 및 정렬 옵션 기능 추가
    * refactor: 검색 바 색상 및 텍스트 색상 조정
    * refactor: 필터 버튼 보더 및 색상 변경

--- app/src/main/java/com/example/swap_it/MainActivity.kt
+++ app/src/main/java/com/example/swap_it/MainActivity.kt
@@ -3,47 +3,14 @@ package com.example.swap_it
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.swap_it.ui.theme.SwapitTheme
+import com.example.swap_it.ui.menu.MenuScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SwapitTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background,
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MenuScreen().MenuScreen()
         }
     }
 }
 
-@Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier,
-) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SwapitTheme {
-        Greeting("Android")
-    }
-}
