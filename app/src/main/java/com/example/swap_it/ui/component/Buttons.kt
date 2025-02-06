87f214da9c73d8f6685bf1abdbe80d99131d9531
SangHyun Ko <87695921+kkosang@users.noreply.github.com>
2025-02-06 14:58:37 +0900

    [UI] 로그인 화면 구현 (#11)
    * feat: 로고 drawable 추가
    
    * feat: 로그인 화면 UI 구현
    
    * chore: ktFormat
    
    * config: pr 템플릿 디렉토리 위치 이동

--- app/src/main/java/com/example/swap_it/ui/component/Buttons.kt
+++ app/src/main/java/com/example/swap_it/ui/component/Buttons.kt
@@ -35,12 +35,13 @@ fun DefaultButton(
     modifier: Modifier = Modifier,
     enabled: Boolean = false,
     contentStyle: TextStyle = Typography.titleMedium,
-    contentPadding: PaddingValues = PaddingValues(
-        horizontal = Paddings.xextra,
-        vertical = Paddings.xlarge,
-    ),
+    contentPadding: PaddingValues =
+        PaddingValues(
+            horizontal = Paddings.xextra,
+            vertical = Paddings.xlarge,
+        ),
     interactionSource: InteractionSource,
-    onClick: () -> Unit
+    onClick: () -> Unit,
 ) {
     val isPressed by interactionSource.collectIsPressedAsState()
 
@@ -49,11 +50,12 @@ fun DefaultButton(
         enabled = enabled,
         modifier = modifier,
         contentPadding = contentPadding,
-        colors = ButtonDefaults.buttonColors(
-            containerColor = if (isPressed) PrimaryDark else Primary,
-            contentColor = White,
-            disabledContainerColor = Gray5,
-        ),
+        colors =
+            ButtonDefaults.buttonColors(
+                containerColor = if (isPressed) PrimaryDark else Primary,
+                contentColor = White,
+                disabledContainerColor = Gray5,
+            ),
         shape = Shapes.small,
     ) {
         Text(
@@ -68,37 +70,41 @@ fun ModalButton(
     text: String,
     modifier: Modifier = Modifier,
     contentStyle: TextStyle = Typography.titleMedium,
-    contentPadding: PaddingValues = PaddingValues(
-        horizontal = Paddings.extra,
-        vertical = Paddings.large,
-    ),
+    contentPadding: PaddingValues =
+        PaddingValues(
+            horizontal = Paddings.extra,
+            vertical = Paddings.large,
+        ),
     containerColor: Color = Primary,
     contentColor: Color = White,
-    onClick: () -> Unit
+    onClick: () -> Unit,
 ) {
     val interactionSource = remember { MutableInteractionSource() }
     val isPressed by interactionSource.collectIsPressedAsState()
     val animatedContainerColor by animateColorAsState(
-        targetValue = when (containerColor) {
-            Primary -> if (isPressed) PrimaryDark else containerColor
-            else -> if (isPressed) Gray4 else containerColor
-        }
+        targetValue =
+            when (containerColor) {
+                Primary -> if (isPressed) PrimaryDark else containerColor
+                else -> if (isPressed) Gray4 else containerColor
+            },
     )
     val animatedContentColor by animateColorAsState(
-        targetValue = when (animatedContainerColor) {
-            Primary, PrimaryDark -> White
-            else -> Gray3
-        },
+        targetValue =
+            when (animatedContainerColor) {
+                Primary, PrimaryDark -> White
+                else -> Gray3
+            },
     )
 
     Button(
         onClick = onClick,
         modifier = modifier,
         contentPadding = contentPadding,
-        colors = ButtonDefaults.buttonColors(
-            containerColor = animatedContainerColor,
-            contentColor = animatedContentColor
-        ),
+        colors =
+            ButtonDefaults.buttonColors(
+                containerColor = animatedContainerColor,
+                contentColor = animatedContentColor,
+            ),
         interactionSource = interactionSource,
         shape = Shapes.small,
     ) {
@@ -115,31 +121,33 @@ fun CategoryButton(
     modifier: Modifier = Modifier,
     isSelected: Boolean,
     contentStyle: TextStyle = Typography.bodySmall,
-    contentPadding: PaddingValues = PaddingValues(
-        horizontal = Paddings.xlarge,
-        vertical = Paddings.medium,
-    ),
+    contentPadding: PaddingValues =
+        PaddingValues(
+            horizontal = Paddings.xlarge,
+            vertical = Paddings.medium,
+        ),
     containerColor: Color = White,
     contentColor: Color = Gray4,
-    onClick: () -> Unit
+    onClick: () -> Unit,
 ) {
     val animatedBorderColor by animateColorAsState(
-        targetValue = if (isSelected) Primary else contentColor
+        targetValue = if (isSelected) Primary else contentColor,
     )
     val animatedContentColor by animateColorAsState(
-        targetValue = if (isSelected) Primary else contentColor
+        targetValue = if (isSelected) Primary else contentColor,
     )
 
     Button(
         onClick = onClick,
         modifier = modifier,
         contentPadding = contentPadding,
-        colors = ButtonDefaults.buttonColors(
-            containerColor = containerColor,
-            contentColor = animatedContentColor
-        ),
+        colors =
+            ButtonDefaults.buttonColors(
+                containerColor = containerColor,
+                contentColor = animatedContentColor,
+            ),
         shape = Shapes.medium,
-        border = BorderStroke(1.dp, animatedBorderColor)
+        border = BorderStroke(1.dp, animatedBorderColor),
     ) {
         Text(
             text = text,
@@ -149,37 +157,39 @@ fun CategoryButton(
 }
 
 class EnabledPreviewParameterProvider : PreviewParameterProvider<Boolean> {
-    override val values = sequenceOf(
-        true,
-        false,
-    )
+    override val values =
+        sequenceOf(
+            true,
+            false,
+        )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultButtonPreview(
-    @PreviewParameter(EnabledPreviewParameterProvider::class) enabled: Boolean
+    @PreviewParameter(EnabledPreviewParameterProvider::class) enabled: Boolean,
 ) {
     DefaultButton(
         text = if (enabled) "활성화된 버튼" else "비활성화된 버튼",
         onClick = {},
         contentStyle = Typography.titleMedium,
         enabled = enabled,
-        interactionSource = remember { MutableInteractionSource() }
+        interactionSource = remember { MutableInteractionSource() },
     )
 }
 
 class ModalButtonPreviewParameterProvider : PreviewParameterProvider<Color> {
-    override val values = sequenceOf(
-        Primary,
-        Gray5,
-    )
+    override val values =
+        sequenceOf(
+            Primary,
+            Gray5,
+        )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun ModalButtonPreview(
-    @PreviewParameter(ModalButtonPreviewParameterProvider::class) containerColor: Color
+    @PreviewParameter(ModalButtonPreviewParameterProvider::class) containerColor: Color,
 ) {
     ModalButton(
         text = "모달 버튼",
@@ -194,6 +204,6 @@ fun CategoryButtonPreview() {
     CategoryButton(
         text = "카테고리 버튼",
         onClick = {},
-        isSelected = false
+        isSelected = false,
     )
 }
--- app/src/main/java/com/example/swap_it/ui/theme/Type.kt
+++ app/src/main/java/com/example/swap_it/ui/theme/Type.kt
@@ -8,95 +8,109 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.example.swap_it.R
 
-val pretendard = FontFamily(
-    Font(R.font.pretendard_bold, FontWeight.Bold),
-    Font(R.font.pretendard_regular, FontWeight.Normal),
-    Font(R.font.pretendard_light, FontWeight.Light),
-)
+val pretendard =
+    FontFamily(
+        Font(R.font.pretendard_bold, FontWeight.Bold),
+        Font(R.font.pretendard_regular, FontWeight.Normal),
+        Font(R.font.pretendard_light, FontWeight.Light),
+    )
 
-val Typography = Typography(
-    headlineLarge = TextStyle(
-        fontFamily = pretendard,
-        fontSize = 36.sp,
-        fontWeight = FontWeight.Bold,
-        letterSpacing = (-0.25).sp,
-        lineHeight = 54.sp
-    ),
-    headlineMedium = TextStyle(
-        fontFamily = pretendard,
-        fontSize = 24.sp,
-        fontWeight = FontWeight.Bold,
-        letterSpacing = (-0.25).sp,
-        lineHeight = 36.sp
-    ),
-    headlineSmall = TextStyle(
-        fontFamily = pretendard,
-        fontSize = 21.sp,
-        fontWeight = FontWeight.Bold,
-        letterSpacing = (-0.25).sp,
-        lineHeight = 31.5.sp
-    ),
-    titleLarge = TextStyle(
-        fontFamily = pretendard,
-        fontSize = 18.sp,
-        fontWeight = FontWeight.Bold,
-        letterSpacing = (-0.25).sp,
-        lineHeight = 26.1.sp
-    ),
-    titleMedium = TextStyle(
-        fontFamily = pretendard,
-        fontSize = 16.sp,
-        fontWeight = FontWeight.Bold,
-        letterSpacing = (-0.25).sp,
-        lineHeight = 24.sp
-    ),
-    titleSmall = TextStyle(
-        fontFamily = pretendard,
-        fontSize = 14.sp,
-        fontWeight = FontWeight.Bold,
-        letterSpacing = (-0.25).sp,
-        lineHeight = 18.2.sp
-    ),
-    bodyLarge = TextStyle(
-        fontFamily = pretendard,
-        fontSize = 18.sp,
-        fontWeight = FontWeight.Normal,
-        letterSpacing = (-0.25).sp,
-        lineHeight = 26.1.sp
-    ),
-    bodyMedium = TextStyle(
-        fontFamily = pretendard,
-        fontSize = 16.sp,
-        fontWeight = FontWeight.Normal,
-        letterSpacing = (-0.25).sp,
-        lineHeight = 22.4.sp
-    ),
-    bodySmall = TextStyle(
-        fontFamily = pretendard,
-        fontSize = 14.sp,
-        fontWeight = FontWeight.Normal,
-        letterSpacing = (-0.25).sp,
-        lineHeight = 18.2.sp
-    ),
-    labelLarge = TextStyle(
-        fontFamily = pretendard,
-        fontSize = 12.sp,
-        fontWeight = FontWeight.Normal,
-        letterSpacing = (-0.25).sp,
-        lineHeight = 15.6.sp
-    ),
-    labelMedium = TextStyle(
-        fontFamily = pretendard,
-        fontSize = 11.sp,
-        fontWeight = FontWeight.Light,
-        letterSpacing = (-0.25).sp,
-        lineHeight = 15.95.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = pretendard,
-        fontSize = 10.sp,
-        fontWeight = FontWeight.Light,
-        letterSpacing = (-0.25).sp,
-        lineHeight = 14.sp
-    ),
-)
+val Typography =
+    Typography(
+        headlineLarge =
+            TextStyle(
+                fontFamily = pretendard,
+                fontSize = 36.sp,
+                fontWeight = FontWeight.Bold,
+                letterSpacing = (-0.25).sp,
+                lineHeight = 54.sp,
+            ),
+        headlineMedium =
+            TextStyle(
+                fontFamily = pretendard,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                letterSpacing = (-0.25).sp,
+                lineHeight = 36.sp,
+            ),
+        headlineSmall =
+            TextStyle(
+                fontFamily = pretendard,
+                fontSize = 21.sp,
+                fontWeight = FontWeight.Bold,
+                letterSpacing = (-0.25).sp,
+                lineHeight = 31.5.sp,
+            ),
+        titleLarge =
+            TextStyle(
+                fontFamily = pretendard,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                letterSpacing = (-0.25).sp,
+                lineHeight = 26.1.sp,
+            ),
+        titleMedium =
+            TextStyle(
+                fontFamily = pretendard,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                letterSpacing = (-0.25).sp,
+                lineHeight = 24.sp,
+            ),
+        titleSmall =
+            TextStyle(
+                fontFamily = pretendard,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                letterSpacing = (-0.25).sp,
+                lineHeight = 18.2.sp,
+            ),
+        bodyLarge =
+            TextStyle(
+                fontFamily = pretendard,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Normal,
+                letterSpacing = (-0.25).sp,
+                lineHeight = 26.1.sp,
+            ),
+        bodyMedium =
+            TextStyle(
+                fontFamily = pretendard,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Normal,
+                letterSpacing = (-0.25).sp,
+                lineHeight = 22.4.sp,
+            ),
+        bodySmall =
+            TextStyle(
+                fontFamily = pretendard,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Normal,
+                letterSpacing = (-0.25).sp,
+                lineHeight = 18.2.sp,
+            ),
+        labelLarge =
+            TextStyle(
+                fontFamily = pretendard,
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Normal,
+                letterSpacing = (-0.25).sp,
+                lineHeight = 15.6.sp,
+            ),
+        labelMedium =
+            TextStyle(
+                fontFamily = pretendard,
+                fontSize = 11.sp,
+                fontWeight = FontWeight.Light,
+                letterSpacing = (-0.25).sp,
+                lineHeight = 15.95.sp,
+            ),
+        labelSmall =
+            TextStyle(
+                fontFamily = pretendard,
+                fontSize = 10.sp,
+                fontWeight = FontWeight.Light,
+                letterSpacing = (-0.25).sp,
+                lineHeight = 14.sp,
+            ),
+    )
