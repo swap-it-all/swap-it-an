4a62788cfb1dcbfe73421edb14310cae47b77e7d
조형석 <chs9902@naver.com>
2025-02-07 18:08:56 +0900

    feat: 추가적인 패딩 값 정의

--- app/src/main/java/com/example/swap_it/ui/theme/Paddings.kt
+++ app/src/main/java/com/example/swap_it/ui/theme/Paddings.kt
@@ -6,7 +6,9 @@ object Paddings {
     val none = 0.dp
     val xsmall = 2.dp
     val small = 4.dp
+    val smallMedium = 6.dp
     val medium = 8.dp
+    val mediumLarge = 10.dp
     val large = 12.dp
     val xlarge = 16.dp
     val extra = 24.dp
