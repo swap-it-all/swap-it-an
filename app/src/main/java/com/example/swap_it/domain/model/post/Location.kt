04ab8cc2bbc8e881a6b90499bd699e2a2e9959f4
kkosang <eiwoqp77@gmail.com>
2025-02-10 15:11:35 +0900

    feat: 물건 스왑 위치 검증

---
+++ app/src/main/java/com/example/swap_it/domain/model/post/Location.kt
@@ -0,0 +1,18 @@
+package com.example.swap_it.domain.model.post
+
+data class Location(private val value: String = DEFAULT_VALUE) {
+    fun validationResult(): ValidationResult {
+        return when {
+            value.isEmpty() -> ValidationResult.EMPTY
+            !isValidRange() -> ValidationResult.INVALID_RANGE
+            else -> ValidationResult.SUCCESS
+        }
+    }
+
+    private fun isValidRange(): Boolean = value.length in LOCATION_RANGE
+
+    companion object {
+        const val DEFAULT_VALUE = "0"
+        val LOCATION_RANGE = 2..20
+    }
+}
