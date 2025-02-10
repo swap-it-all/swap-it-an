567c8b79646e8a4a4552e94dcffc536cafc7f991
kkosang <eiwoqp77@gmail.com>
2025-02-10 14:45:31 +0900

    feat: 물건 등록시 품질 option 추가

---
+++ app/src/main/java/com/example/swap_it/data/datasource/local/model/post/QualityOption.kt
@@ -0,0 +1,9 @@
+package com.example.swap_it.data.datasource.local.model.post
+
+enum class QualityOption(val option: String) {
+    NEW("새 상품"),
+    EXCELLENT("최상"),
+    GOOD("상"),
+    FAIR("중"),
+    POOR("하"),
+}
