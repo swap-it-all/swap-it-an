091a0ea5a77f2e9615365c4a2215f31236f09691
kkosang <eiwoqp77@gmail.com>
2025-02-10 13:43:04 +0900

    refactor: 텍스트필드 수정

--- app/src/main/java/com/example/swap_it/ui/component/TextFields.kt
+++ app/src/main/java/com/example/swap_it/ui/component/TextFields.kt
@@ -1,35 +1,44 @@
 package com.example.swap_it.ui.component
 
-import androidx.compose.foundation.border
-import androidx.compose.material3.TextField
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.example.swap_it.ui.theme.BackgroundColor
 import com.example.swap_it.ui.theme.Gray3
 import com.example.swap_it.ui.theme.Gray4
 import com.example.swap_it.ui.theme.Shapes
 import com.example.swap_it.ui.theme.Typography
-import com.example.swap_it.ui.theme.White
 
 @Composable
 fun DefaultTextField(
     value: String,
     onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier,
+    singleLine: Boolean = false,
+    placeholder: @Composable (() -> Unit)? = null,
+    supportingText: @Composable (() -> Unit)? = null,
+    isError: Boolean = false,
 ) {
-    TextField(
+    OutlinedTextField(
         value = value,
         onValueChange = { onValueChange(it) },
-        modifier = modifier.border(1.dp, color = Gray4, shape = Shapes.medium),
+        modifier = modifier,
+        shape = Shapes.medium,
         textStyle = Typography.bodyMedium,
         colors = TextFieldDefaults.colors(
-            focusedContainerColor = White,  // todo color change
-            unfocusedContainerColor = White,
+            focusedContainerColor = BackgroundColor,
+            unfocusedContainerColor = BackgroundColor,
             focusedTextColor = Gray3,
             unfocusedTextColor = Gray3,
-        )
+            focusedIndicatorColor = Gray4,
+            unfocusedIndicatorColor = Gray4,
+        ),
+        singleLine = singleLine,
+        placeholder = placeholder,
+        supportingText = supportingText,
+        isError = isError,
     )
 }
 
