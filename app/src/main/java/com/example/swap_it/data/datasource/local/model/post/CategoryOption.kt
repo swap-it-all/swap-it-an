4a352813b7cda51df87be75e1e824d3419c8bf84
kkosang <eiwoqp77@gmail.com>
2025-02-10 14:43:29 +0900

    feat: 물건 등록시 카테고리 option 추가

---
+++ app/src/main/java/com/example/swap_it/data/datasource/local/model/post/CategoryOption.kt
@@ -0,0 +1,16 @@
+package com.example.swap_it.data.datasource.local.model.post
+
+enum class CategoryOption(val option: String) {
+    ELECTRONICS("전자기기"),
+    APPLIANCES("가전제품"),
+    FURNITURE("가구"),
+    CLOTHING("의류/패션"),
+    BEAUTY("뷰티/화장품"),
+    SPORTS("스포츠/레저"),
+    TOYS("장난감/키즈"),
+    BOOKS("책/문구"),
+    AUTOMOTIVE("자동차/부품"),
+    PET("반려동물용품"),
+    GROCERIES("식품/음료"),
+    MISC("기타"),
+}
