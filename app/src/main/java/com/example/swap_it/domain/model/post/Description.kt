c5515b439c4a0c88bafa6da8e90ce1bb67063892
kkosang <eiwoqp77@gmail.com>
2025-02-10 15:00:15 +0900

    feat: 물건 등록시 설명 검증 구현

---
+++ app/src/main/java/com/example/swap_it/domain/model/post/Description.kt
@@ -0,0 +1,18 @@
+package com.example.swap_it.domain.model.post
+
+data class Description(val value: String = DEFAULT_VALUE) {
+    fun validationResult(): ValidationResult {
+        return when {
+            value.isEmpty() -> ValidationResult.EMPTY
+            !isValidRange() -> ValidationResult.INVALID_RANGE
+            else -> ValidationResult.SUCCESS
+        }
+    }
+
+    private fun isValidRange(): Boolean = value.length in DESCRIPTION_RANGE
+
+    companion object {
+        const val DEFAULT_VALUE = "0"
+        val DESCRIPTION_RANGE = 0..300
+    }
+}
--- app/src/main/java/com/example/swap_it/ui/post/component/PostProductPriceTextField.kt
+++ app/src/main/java/com/example/swap_it/ui/post/component/PostProductPriceTextField.kt
@@ -1,6 +1,5 @@
 package com.example.swap_it.ui.post.component
 
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
