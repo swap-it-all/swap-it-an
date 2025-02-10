04c7352ac2738a33e329fb49ccfe3cb5ec802a8a
kkosang <eiwoqp77@gmail.com>
2025-02-10 14:53:18 +0900

    feat: 물건 등록시 이름 검증 구현

---
+++ app/src/main/java/com/example/swap_it/domain/model/post/Name.kt
@@ -0,0 +1,19 @@
+package com.example.swap_it.domain.model.post
+
+data class Name(private val value: String = DEFAULT_VALUE) {
+
+    fun validationResult(): ValidationResult {
+        return when {
+            value.isEmpty() -> ValidationResult.EMPTY
+            !isValidLength() -> ValidationResult.INVALID_LENGTH
+            else -> ValidationResult.SUCCESS
+        }
+    }
+
+    private fun isValidLength(): Boolean = value.length in PRODUCT_NAME_RANGE
+
+    companion object {
+        const val DEFAULT_VALUE = ""
+        val PRODUCT_NAME_RANGE = 2..20
+    }
+}
---
+++ app/src/main/java/com/example/swap_it/domain/model/post/ValidationResult.kt
@@ -0,0 +1,9 @@
+package com.example.swap_it.domain.model.post
+
+enum class ValidationResult {
+    EMPTY,
+    INVALID_LENGTH,
+    INVALID_RANGE,
+    INVALID_FORMAT,
+    SUCCESS,
+}
