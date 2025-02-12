90fedb43847b52a3f6436b48d1f515ebb5e73662
kkosang <eiwoqp77@gmail.com>
2025-02-12 21:43:40 +0900

    config: serilization plugins 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     alias(libs.plugins.ktlint)
+    alias(libs.plugins.kotlinx.serilization)
 }
 
 val properties =
