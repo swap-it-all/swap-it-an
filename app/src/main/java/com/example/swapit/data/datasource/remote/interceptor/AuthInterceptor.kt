128d86e95b020abb840eee3fb923143d3e791403
SangHyun Ko <87695921+kkosang@users.noreply.github.com>
2025-02-19 20:32:19 +0900

    [FEAT] 물건 등록 API 연동 및 토큰 관리 (#42)
    * feat: ViewModel을 ViewModelProvider로 생성
    
    * feat: 로그인 api 서비스 추가
    
    * feat: 로그인 repository 구현
    
    * feat: 로그인 dataSource 구현
    
    * feat: 로그인 JWT 토큰 저장
    
    * feat: SAA + Navigation 구현
    
    * feat: 로그아웃 api 연결
    
    * feat: 구글 로그인 api 연결
    
    * chore: ktFormat
    
    * refactor: prefs key 상수화
    
    * refactor: Login으로 네이밍 통일
    
    * chore: ktFormat
    
    * feat: ProductService api 서비스 추가
    
    * feat: ProductRequest DTO 추가
    
    * feat: ProductRequest DTO 변경
    
    * refactor: productService 수정
    
    * feat: dataSource,Repository 생성
    
    * feat: 상품 등록 api 연동 및 화면 이동
    
    * feat: 리프레시 토큰 관리
    
    * chore: ktFormat
    
    * chore: 불필요한 주석 삭제
    
    * chore: 머지 충돌 해결
    
    * chore: ktFormat

--- app/src/main/java/com/example/swapit/data/datasource/remote/interceptor/AuthInterceptor.kt
+++ app/src/main/java/com/example/swapit/data/datasource/remote/interceptor/AuthInterceptor.kt
@@ -4,16 +4,22 @@ import com.example.swapit.data.datasource.local.LocalLoginDataSource
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class AuthInterceptor(private val localLoginDataSource: LocalLoginDataSource) : Interceptor {
+class AuthInterceptor(
+    private val localLoginDataSource: LocalLoginDataSource,
+) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
         val accessToken = localLoginDataSource.accessToken()
 
-        val request =
-            chain.request().newBuilder()
-                .apply {
+        val isRefreshRequest = request.url.encodedPath.contains("/refresh")
+
+        val newRequest =
+            request.newBuilder().apply {
+                if (!isRefreshRequest) {
                     accessToken?.let { addHeader("Authorization", "Bearer $it") }
                 }
-                .build()
-        return chain.proceed(request)
+            }.build()
+
+        return chain.proceed(newRequest)
     }
 }
