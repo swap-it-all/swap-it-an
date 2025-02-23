b14fc3194f4260173bb77d8c890547c9eb0ee456
조형석 <chs9902@naver.com>
2025-02-23 14:04:36 +0900

    refactor: 기존 버튼에서 앱바로 변경

--- app/src/main/java/com/example/swapit/ui/alert/AlertScreen.kt
+++ app/src/main/java/com/example/swapit/ui/alert/AlertScreen.kt
@@ -1,21 +1,32 @@
 package com.example.swapit.ui.alert
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.swapit.R
+import com.example.swapit.ui.chat._chatCardData
+import com.example.swapit.ui.chat.room.ChatRoomAppBar
 import com.example.swapit.ui.component.BackButton
+import com.example.swapit.ui.component.MenuButton
 import com.example.swapit.ui.theme.BackgroundColor
 import com.example.swapit.ui.theme.Black
 import com.example.swapit.ui.theme.Paddings
@@ -25,27 +36,31 @@ import com.example.swapit.ui.theme.Typography
 @Composable
 fun AlertScreen(navController: NavHostController) {
     val alertCardDataList = 1
-    Column(
-        Modifier
-            .fillMaxSize()
-            .background(BackgroundColor),
-    ) {
-        Spacer(Modifier.padding(Paddings.xlarge))
-        BackButton(Modifier.padding(Paddings.largeExtra), navController, color = Black)
-        Text(
-            stringResource(R.string.alert_alert),
-            style = Typography.titleLarge,
-            modifier = Modifier.padding(Paddings.xlarge),
-        )
-        if (alertCardDataList == 0) {
-            NoAlertIconSection()
-        }
-        LazyColumn(horizontalAlignment = Alignment.CenterHorizontally) {
-            items(4) {
-                AlertCard(alertCardData = alertCardData)
+    Scaffold (
+        topBar = {AlertAppBar(navController)}
+    ){contentPadding ->
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(contentPadding)
+                .background(BackgroundColor),
+        ) {
+            Text(
+                stringResource(R.string.alert_alert),
+                style = Typography.titleLarge,
+                modifier = Modifier.padding(Paddings.xlarge),
+            )
+            if (alertCardDataList == 0) {
+                NoAlertIconSection()
+            }
+            LazyColumn(horizontalAlignment = Alignment.CenterHorizontally) {
+                items(4) {
+                    AlertCard(alertCardData = alertCardData)
+                }
             }
         }
     }
+
 }
 
 @Preview(showBackground = true)
@@ -56,3 +71,6 @@ fun SearchScreenPreview() {
         AlertScreen(navController)
     }
 }
+
+
+
