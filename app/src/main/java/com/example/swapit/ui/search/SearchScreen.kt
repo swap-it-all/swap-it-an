8938567927076e3dd808f45f6dcb9c578646d869
조형석 <chs9902@naver.com>
2025-02-23 14:12:33 +0900

    refactor: TopAppBar로 변경

--- app/src/main/java/com/example/swapit/ui/search/SearchScreen.kt
+++ app/src/main/java/com/example/swapit/ui/search/SearchScreen.kt
@@ -8,7 +8,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -35,59 +39,45 @@ fun SearchScreen(
     viewModel: SearchViewModel,
 ) {
     var searchTerm by remember { mutableStateOf("") }
-    Column(
-        Modifier
-            .fillMaxSize()
-            .background(BackgroundColor),
-    ) {
-        Spacer(Modifier.size(32.dp))
-        Row(
-            modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .padding(
-                        Paddings.none,
-                        Paddings.none,
-                        Paddings.xlarge,
-                        Paddings.xlarge,
-                    ),
-            verticalAlignment = Alignment.CenterVertically,
+    Scaffold (topBar = {
+        SearchAppBar(navController, searchTerm, onValueChange = {searchTerm = it})
+    }){contentPadding ->
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(contentPadding)
+                .background(BackgroundColor),
         ) {
-            BackButton(Modifier.padding(Paddings.medium), navController, color = Black)
-            SearchField(
-                searchTerm,
-                Modifier,
-                onValueChange = {
-                    searchTerm = it
-                },
-            )
-        }
-        Text(
-            stringResource(R.string.search_current_term),
-            style = Typography.titleLarge,
-            modifier =
+            Text(
+                stringResource(R.string.search_current_term),
+                style = Typography.titleLarge,
+                modifier =
                 Modifier.padding(
                     Paddings.xlarge,
                     Paddings.large,
                     Paddings.none,
                     Paddings.smallMedium,
                 ),
-        )
-        RecentTermButtonField(viewModel)
-        Text(
-            stringResource(R.string.search_popular_term),
-            style = Typography.titleLarge,
-            modifier =
+            )
+            RecentTermButtonField(viewModel)
+            Text(
+                stringResource(R.string.search_popular_term),
+                style = Typography.titleLarge,
+                modifier =
                 Modifier.padding(
                     Paddings.xlarge,
                     Paddings.xextra,
                     Paddings.none,
                     Paddings.smallMedium,
                 ),
-        )
-        PopularTermButtonField(viewModel)
+            )
+            PopularTermButtonField(viewModel)
+        }
     }
+
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
