2c40d8af8a1126681bb793d4de7d7cb46e00c63c
조형석 <chs9902@naver.com>
2025-02-26 18:22:42 +0900

    feat: 물물교환 요청 시, 나의 물건 목록 추가

--- app/src/main/java/com/example/swapit/ui/shopping/detail/select/MyProductSelectionScreen.kt
+++ app/src/main/java/com/example/swapit/ui/shopping/detail/select/MyProductSelectionScreen.kt
@@ -1,5 +1,6 @@
 package com.example.swapit.ui.shopping.detail.select
 
+import ShoppingViewModel
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,10 +13,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.swapit.domain.repository.ShoppingRepository
 import com.example.swapit.ui.theme.BackgroundColor
 
 @Composable
-fun MyProductSelectionScreen(navController: NavHostController) {
+fun MyProductSelectionScreen(navController: NavHostController,viewModel: ShoppingViewModel) {
     var openDialog by remember { mutableStateOf(false) }
     Column(
         Modifier
@@ -23,7 +25,7 @@ fun MyProductSelectionScreen(navController: NavHostController) {
             .background(BackgroundColor),
     ) {
         BackButtonAndTextSection(navController)
-        ProductListSection({ openDialog = true }, navController)
+        ProductListSection(viewModel = viewModel,{ openDialog = true })
         DialogSection(openDialog, onClickCancel = { openDialog = false }, navController)
     }
 }
@@ -31,5 +33,5 @@ fun MyProductSelectionScreen(navController: NavHostController) {
 @Preview(showBackground = true)
 @Composable
 fun SwapRequestScreenPreview() {
-    MyProductSelectionScreen(rememberNavController())
+    MyProductSelectionScreen(rememberNavController(),ShoppingViewModel(ShoppingRepository.instance()))
 }
--- app/src/main/java/com/example/swapit/ui/shopping/detail/select/ProductListSection.kt
+++ app/src/main/java/com/example/swapit/ui/shopping/detail/select/ProductListSection.kt
@@ -1,20 +1,22 @@
 package com.example.swapit.ui.shopping.detail.select
 
+import ShoppingViewModel
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.navigation.NavHostController
+import com.example.swapit.data.datasource.toDomainModel
 import com.example.swapit.ui.shopping.ShoppingCard
 import com.example.swapit.ui.shopping.productCardData
 
 @Composable
 fun ProductListSection(
+    viewModel:ShoppingViewModel,
     openDialog: () -> Unit,
-    navController: NavHostController,
 ) {
     LazyColumn(horizontalAlignment = Alignment.CenterHorizontally) {
-        items(4) {
-            ShoppingCard(shoppingCardData = productCardData, navController, {
+        items(viewModel.products.size) {
+            ShoppingCard(viewModel.products[it].toDomainModel(), {
                 openDialog()
             })
         }
