b26271690f600f8a4c595f0be5f8368cb5d7d8d0
kkosang <eiwoqp77@gmail.com>
2025-02-26 23:09:21 +0900

    gradle: minSDK 버전 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.swapit"
-        minSdk = 31
+        minSdk = 29
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
