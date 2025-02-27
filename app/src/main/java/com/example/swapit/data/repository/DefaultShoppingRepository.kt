85507fc592cff8061a67832b205d437a7f058725
조형석 <chs9902@naver.com>
2025-02-27 11:58:57 +0900

    feat: shopping repository 결과 추가

--- app/src/main/java/com/example/swapit/data/repository/DefaultShoppingRepository.kt
+++ app/src/main/java/com/example/swapit/data/repository/DefaultShoppingRepository.kt
@@ -1,29 +1,24 @@
 package com.example.swapit.data.repository
 
-import android.content.Context
-import android.os.Environment
-import coil3.Uri
-import coil3.toAndroidUri
-import com.example.swapit.data.datasource.RemoteProductDataSource
 import com.example.swapit.data.datasource.RemoteShoppingDataSource
-import com.example.swapit.data.datasource.local.model.post.QualityOption
-import com.example.swapit.data.datasource.remote.dto.request.product.ProductRequest
-import com.example.swapit.data.datasource.remote.dto.response.BaseResponse
-import com.example.swapit.data.datasource.remote.dto.response.shopping.GoodsListResponse
-import com.example.swapit.domain.repository.ProductRepository
+import com.example.swapit.domain.model.shopping.ShoppingProduct
+import com.example.swapit.domain.model.shopping.ShoppingProductResponse
+import com.example.swapit.domain.model.shopping.ShoppingProductResults
+import com.example.swapit.domain.model.shopping.toDomainModel
 import com.example.swapit.domain.repository.ShoppingRepository
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.MultipartBody
-import okhttp3.RequestBody.Companion.asRequestBody
-import java.io.File
-import java.io.FileOutputStream
 
 class DefaultShoppingRepository(
     private val remoteSource: RemoteShoppingDataSource,
 ) :
     ShoppingRepository {
-    override suspend fun getShoppingData(): GoodsListResponse {
-        return remoteSource.getShoppingData()
+    override suspend fun getShoppingResponse(): ShoppingProductResponse {
+        return remoteSource.getRemoteShoppingProductResponse()
     }
+
+    override suspend fun getShoppingResults(shoppingProductResponse: ShoppingProductResponse): ShoppingProductResults {
+        val results = shoppingProductResponse.results.toDomainModel()
+        return results
+    }
+
 }
 
