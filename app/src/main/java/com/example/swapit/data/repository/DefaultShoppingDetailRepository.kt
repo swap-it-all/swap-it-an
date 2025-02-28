a4461e5717d8f634377a63ec898bd81414fec007
조형석 <chs9902@naver.com>
2025-02-28 15:27:10 +0900

    feat: 쇼핑 상세 정보 Repository 구현

---
+++ app/src/main/java/com/example/swapit/data/repository/DefaultShoppingDetailRepository.kt
@@ -0,0 +1,17 @@
+package com.example.swapit.data.repository
+
+import com.example.swapit.data.datasource.RemoteShoppingDataSource
+import com.example.swapit.data.datasource.RemoteShoppingDetailDataSource
+import com.example.swapit.data.datasource.remote.dto.response.shopping.detail.ShoppingDetailDataResponse
+import com.example.swapit.domain.model.shopping.detail.ShoppingDetailImage
+import com.example.swapit.domain.model.shopping.detail.ShoppingDetailUser
+import com.example.swapit.domain.repository.ShoppingDetailRepository
+
+class DefaultShoppingDetailRepository(
+    private val remoteSource: RemoteShoppingDetailDataSource,
+) :
+    ShoppingDetailRepository {
+    override suspend fun shoppingDetailResults(goodsId: String): ShoppingDetailDataResponse {
+        return remoteSource.shoppingDetailResponse(goodsId).results
+    }
+}
