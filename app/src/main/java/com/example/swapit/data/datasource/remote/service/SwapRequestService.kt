1ef7ca4b16c6d19499d5ce2e3f709b48b83c169c
조형석 <chs9902@naver.com>
2025-03-04 18:03:09 +0900

    feat: Add SwapRequestService for swap requests

---
+++ app/src/main/java/com/example/swapit/data/datasource/remote/service/SwapRequestService.kt
@@ -0,0 +1,17 @@
+package com.example.swapit.data.datasource.remote.service
+
+import com.example.swapit.data.datasource.remote.dto.request.swap.SwapRequest
+import com.example.swapit.data.datasource.remote.dto.response.BaseResponse
+import retrofit2.http.Body
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+
+interface SwapRequestService {
+    @POST("api/user/swap/request")
+    suspend fun swapRequest(
+        @Body request: SwapRequest
+    ): BaseResponse<Unit>
+}
