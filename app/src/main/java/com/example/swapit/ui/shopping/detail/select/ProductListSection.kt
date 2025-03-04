0624c9a52c430824bb78a7ae8de042874b0df735
조형석 <chs9902@naver.com>
2025-03-04 17:59:38 +0900

    feat: 스왑 요구 기능 추가

--- app/src/main/java/com/example/swapit/ui/shopping/detail/select/ProductListSection.kt
+++ app/src/main/java/com/example/swapit/ui/shopping/detail/select/ProductListSection.kt
@@ -1,27 +1,45 @@
 package com.example.swapit.ui.shopping.detail.select
 
-import ShoppingViewModel
-import android.util.Log
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import com.example.swapit.ui.navigation.NavItem
+import androidx.navigation.NavHostController
 import com.example.swapit.ui.shopping.ShoppingCard
 
 @Composable
 fun ProductListSection(
-    viewModel: MyProductSelectionViewModel,
-    openDialog: () -> Unit,
+    myProductSelectionViewModel: MyProductSelectViewModel,
+    swapViewModel: SwapProductViewModel,
+    targetProductId: Long
 ) {
+    var openDialog by remember { mutableStateOf(false) }
     LazyColumn(horizontalAlignment = Alignment.CenterHorizontally) {
         itemsIndexed(
-            items = viewModel.products,
+            items = myProductSelectionViewModel.products,
             key = { _, item -> item.goodsId },
         ) { _, item ->
             ShoppingCard(item) {
-                openDialog()
+                openDialog = true
             }
+            DialogSection(
+                openDialog,
+                onClickCancel = {
+                    openDialog = false
+                },
+                onClickConfirm = {
+                    openDialog = false
+                    swapViewModel.requestedProductId.longValue = item.goodsId
+                    swapViewModel.targetProductId.longValue = targetProductId
+                    swapViewModel.swapRequest()
+                },
+                item.imageUrl
+            )
         }
     }
+
 }
