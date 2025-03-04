74f319a31e928c93cfca81d92bd1251c8615591a
조형석 <chs9902@naver.com>
2025-03-04 18:02:22 +0900

    feat: 스왑 요청 데이터 소스 추가

---
+++ app/src/main/java/com/example/swapit/data/datasource/RemoteSwapRequestDataSource.kt
@@ -0,0 +1,12 @@
+package com.example.swapit.data.datasource
+
+import com.example.swapit.data.datasource.remote.dto.request.swap.SwapRequest
+import com.example.swapit.data.datasource.remote.dto.response.BaseResponse
+import com.example.swapit.data.datasource.remote.service.SwapRequestService
+
+class RemoteSwapRequestDataSource(private val swapRequestService: SwapRequestService) {
+    suspend fun swapRequest(swapRequest: SwapRequest): BaseResponse<Unit> =
+        swapRequestService.swapRequest(
+            swapRequest
+        )
+}
