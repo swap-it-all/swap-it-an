b58f49caeb640c9b7678bf8acc85d74408c12649
조형석 <chs9902@naver.com>
2025-03-04 18:02:58 +0900

    feat: Add SwapRequestRepository interface

---
+++ app/src/main/java/com/example/swapit/domain/repository/SwapRequestRepository.kt
@@ -0,0 +1,28 @@
+package com.example.swapit.domain.repository
+
+import android.content.Context
+import com.example.swapit.data.datasource.RemoteSwapRequestDataSource
+import com.example.swapit.data.datasource.remote.ServiceModule
+import com.example.swapit.data.datasource.remote.dto.request.swap.SwapRequest
+import com.example.swapit.data.datasource.remote.dto.response.BaseResponse
+import com.example.swapit.data.repository.DefaultSwapRequestRepository
+
+interface SwapRequestRepository {
+    suspend fun swapRequest(
+        swapRequest: SwapRequest
+    ): BaseResponse<Unit>
+
+    companion object {
+        private var instance: SwapRequestRepository? = null
+
+        fun instance(): SwapRequestRepository {
+            if (instance == null) {
+                instance =
+                    DefaultSwapRequestRepository(
+                        remoteSource = RemoteSwapRequestDataSource(ServiceModule.swapRequestService),
+                    )
+            }
+            return instance!!
+        }
+    }
+}
