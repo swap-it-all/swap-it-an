82a35e9c29ef90e633852eb8e32f13666a969fe5
조형석 <chs9902@naver.com>
2025-03-06 02:16:10 +0900

    refactor: 다이얼 로그 상태 관리 뷰모델로 이전

--- app/src/main/java/com/example/swapit/ui/shopping/detail/select/ProductListSection.kt
+++ app/src/main/java/com/example/swapit/ui/shopping/detail/select/ProductListSection.kt
@@ -1,14 +1,10 @@
 package com.example.swapit.ui.shopping.detail.select
 
+import android.util.Log
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.navigation.NavHostController
 import com.example.swapit.ui.shopping.ShoppingCard
 
 @Composable
@@ -17,22 +13,22 @@ fun ProductListSection(
     swapViewModel: SwapProductViewModel,
     targetProductId: Long
 ) {
-    var openDialog by remember { mutableStateOf(false) }
+
     LazyColumn(horizontalAlignment = Alignment.CenterHorizontally) {
         itemsIndexed(
             items = myProductSelectionViewModel.products,
             key = { _, item -> item.goodsId },
         ) { _, item ->
             ShoppingCard(item) {
-                openDialog = true
+                swapViewModel.openDialog(item.goodsId)
             }
             DialogSection(
-                openDialog,
+                swapViewModel.dialogStates.value[item.goodsId] ?: false,
                 onClickCancel = {
-                    openDialog = false
+                    swapViewModel.closeDialog(item.goodsId)
                 },
                 onClickConfirm = {
-                    openDialog = false
+                    swapViewModel.closeDialog(item.goodsId)
                     swapViewModel.requestedProductId.longValue = item.goodsId
                     swapViewModel.targetProductId.longValue = targetProductId
                     swapViewModel.swapRequest()
