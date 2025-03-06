baeee2dfda5a654f75829508d006010eab720b18
조형석 <chs9902@naver.com>
2025-03-07 02:35:54 +0900

    feat: 교환 내역 관련 기능 추가

--- app/src/main/java/com/example/swapit/data/repository/DefaultSwapRepository.kt
+++ app/src/main/java/com/example/swapit/data/repository/DefaultSwapRepository.kt
@@ -3,6 +3,9 @@ package com.example.swapit.data.repository
 import com.example.swapit.data.datasource.RemoteSwapDataSource
 import com.example.swapit.data.datasource.remote.dto.request.swap.SwapRequest
 import com.example.swapit.data.datasource.remote.dto.response.BaseResponse
+import com.example.swapit.data.datasource.remote.dto.response.swap.ReceivedSwapProductsResultResponse
+import com.example.swapit.data.datasource.remote.dto.response.swap.ReceivedSwapResponse
+import com.example.swapit.data.datasource.remote.dto.response.swap.SentSwapResponse
 import com.example.swapit.domain.repository.SwapRepository
 
 class DefaultSwapRepository(
@@ -10,5 +13,17 @@ class DefaultSwapRepository(
 ) : SwapRepository {
     override suspend fun swapRequest(swapRequest: SwapRequest): BaseResponse<Long> {
         return remoteSource.swapRequest(swapRequest)
+    }
+
+    override suspend fun receivedSwap(): BaseResponse<ReceivedSwapResponse> {
+        return remoteSource.receivedSwap()
+    }
+
+    override suspend fun receivedSwapProductsResult(goodsId: Long): BaseResponse<ReceivedSwapProductsResultResponse> {
+        return remoteSource.receivedSwapProductsResult(goodsId)
+    }
+
+    override suspend fun sentSwap(): BaseResponse<SentSwapResponse> {
+        return remoteSource.sentSwap()
     }
 }
