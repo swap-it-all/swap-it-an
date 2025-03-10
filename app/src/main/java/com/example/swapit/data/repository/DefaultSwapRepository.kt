176a753dbba5eddc4d3edc692c92235dff756abf
조형석 <chs9902@naver.com>
2025-03-10 17:09:43 +0900

    feat: 상품 판매, 판매 완료 목록 조회 기능 추가

--- app/src/main/java/com/example/swapit/data/repository/DefaultSwapRepository.kt
+++ app/src/main/java/com/example/swapit/data/repository/DefaultSwapRepository.kt
@@ -1,11 +1,13 @@
 package com.example.swapit.data.repository
 
+import android.util.Log
 import com.example.swapit.data.datasource.RemoteSwapDataSource
 import com.example.swapit.data.datasource.remote.dto.request.swap.SwapRequest
 import com.example.swapit.data.datasource.remote.dto.response.BaseResponse
-import com.example.swapit.data.datasource.remote.dto.response.swap.ReceivedSwapProductsResultResponse
-import com.example.swapit.data.datasource.remote.dto.response.swap.ReceivedSwapResponse
-import com.example.swapit.data.datasource.remote.dto.response.swap.SentSwapResponse
+import com.example.swapit.data.mapper.toDomain
+import com.example.swapit.domain.model.swap.ReceivedSwap
+import com.example.swapit.domain.model.swap.ReceivedSwapProductsResult
+import com.example.swapit.domain.model.swap.SentSwap
 import com.example.swapit.domain.repository.SwapRepository
 
 class DefaultSwapRepository(
@@ -15,15 +17,30 @@ class DefaultSwapRepository(
         return remoteSource.swapRequest(swapRequest)
     }
 
-    override suspend fun receivedSwap(): BaseResponse<ReceivedSwapResponse> {
-        return remoteSource.receivedSwap()
+    override suspend fun receivedSwap(): List<ReceivedSwap> {
+        if (remoteSource.receivedSwap().success){
+            Log.d("SwapRepository", "Received swap success")
+        } else {
+            Log.e("SwapRepository", "Received swap failed")
+        }
+        return remoteSource.receivedSwap().results.goodsList.map { it.toDomain() }
     }
 
-    override suspend fun receivedSwapProductsResult(goodsId: Long): BaseResponse<ReceivedSwapProductsResultResponse> {
-        return remoteSource.receivedSwapProductsResult(goodsId)
+    override suspend fun receivedSwapProductsResult(goodsId: Long): ReceivedSwapProductsResult {
+        if (remoteSource.receivedSwapProductsResult(goodsId).success){
+            Log.d("SwapRepository", "Received swap products result success")
+        } else {
+            Log.e("SwapRepository", "Received swap products result failed")
+        }
+        return remoteSource.receivedSwapProductsResult(goodsId).results.toDomain()
     }
 
-    override suspend fun sentSwap(): BaseResponse<SentSwapResponse> {
-        return remoteSource.sentSwap()
+    override suspend fun sentSwap(): List<SentSwap> {
+        if (remoteSource.sentSwap().success){
+            Log.d("SwapRepository", "Sent swap success")
+        } else {
+            Log.e("SwapRepository", "Sent swap failed")
+        }
+        return remoteSource.sentSwap().results.goodsList.map { it.toDomain() }
     }
 }
