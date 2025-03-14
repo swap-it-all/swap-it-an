5e92fc29efe757e4c218348a2b7f2f4f679cfbc0
조형석 <chs9902@naver.com>
2025-03-14 16:22:45 +0900

    feat: 채팅 서비스 추가

---
+++ app/src/main/java/com/example/swapit/data/datasource/remote/service/ChatService.kt
@@ -0,0 +1,40 @@
+package com.example.swapit.data.datasource.remote.service
+
+import com.example.swapit.data.datasource.remote.dto.request.chat.GoodsIdRequest
+import com.example.swapit.data.datasource.remote.dto.request.chat.TradesIdRequest
+import com.example.swapit.data.datasource.remote.dto.response.BaseResponse
+import com.example.swapit.data.datasource.remote.dto.response.chat.ChatListResponse
+import com.example.swapit.data.datasource.remote.dto.response.chat.ChatRoomListResponse
+import com.example.swapit.data.datasource.remote.dto.response.chat.ChatRoomProductResponse
+import com.example.swapit.data.datasource.remote.dto.response.login.LoginResponse
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface ChatService {
+    @POST("api/user/chatroom")
+    suspend fun createChatRoom(
+        @Body goodsId: GoodsIdRequest,
+    ): BaseResponse<Long>
+
+    @POST("api/user/swap-chatroom")
+    suspend fun createSwapChatRoom(
+        @Body tradesId: TradesIdRequest,
+    ): BaseResponse<Long>
+
+    @GET("api/user/chatroom")
+    suspend fun chatRoomList(
+    ): BaseResponse<ChatRoomListResponse>
+
+    @GET("api/user/chatroom/{chatroomId}")
+    suspend fun chatList(
+        @Path("chatroomId") chatroomId: Long,
+    ): BaseResponse<ChatListResponse>
+
+    @GET("api/user/chatroom/{chatroomId}/goods")
+    suspend fun chatRoomProduct(
+        @Path("chatroomId") chatroomId: Long,
+    ): BaseResponse<ChatRoomProductResponse>
+}
