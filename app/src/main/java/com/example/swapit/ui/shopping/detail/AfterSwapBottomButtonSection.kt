909f96d2f14b684d4a2847f650ffaa1028bb3e17
조형석 <chs9902@naver.com>
2025-03-14 16:55:53 +0900

    feat: 스왑 완료 후 하단 버튼 및 관련 타입 추가

---
+++ app/src/main/java/com/example/swapit/ui/shopping/detail/AfterSwapBottomButtonSection.kt
@@ -0,0 +1,47 @@
+package com.example.swapit.ui.shopping.detail
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.swapit.R
+import com.example.swapit.ui.component.DefaultButton
+import com.example.swapit.ui.component.ModalButton
+import com.example.swapit.ui.swap.SwapViewModel
+import com.example.swapit.ui.theme.Gray5
+import com.example.swapit.ui.theme.Paddings
+
+@Composable
+fun AfterSwapBottomButtonSection(
+    viewModel: ShoppingDetailViewModel,
+    swapViewModel: SwapViewModel
+) {
+    val configuration = LocalConfiguration.current
+    val screenWidthDp = configuration.screenWidthDp.dp
+    val horizontalPadding = screenWidthDp.value / 20
+    ModalButton(
+        text = "스왑 요청 취소하기",
+        contentPadding =
+        PaddingValues(
+            horizontal = horizontalPadding.dp,
+            vertical = Paddings.xlarge,
+        ),
+        containerColor = Gray5,
+    ) {
+        swapViewModel.swapCancel(viewModel.detailContents.trade!!.tradesId)
+    }
+    DefaultButton(
+        text = stringResource(R.string.shopping_detail_chat_bottom_button),
+        enabled = true,
+        modifier = Modifier.padding(start = Paddings.large),
+        contentPadding =
+        PaddingValues(
+            horizontal = horizontalPadding.dp * 2,
+            vertical = Paddings.xlarge,
+        ),
+    ) {
+    }
+}
