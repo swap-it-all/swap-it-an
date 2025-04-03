f77e575d9daea46e7c23db4a5ef93c25776f41ab
조형석 <chs9902@naver.com>
2025-04-03 21:51:54 +0900

    style: ktlint

--- app/src/main/java/com/swapit/company/ui/shopping/detail/ShoppingDetailScreen.kt
+++ app/src/main/java/com/swapit/company/ui/shopping/detail/ShoppingDetailScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.swapit.company.data.datasource.local.model.shopping.ProductTradeStatus
 import com.swapit.company.data.datasource.local.model.shopping.TradeStatus
 import com.swapit.company.ui.chat.ChatViewModel
 import com.swapit.company.ui.shopping.detail.bottom.AfterSwapBottomButtonSection
@@ -50,50 +51,53 @@ fun ShoppingDetailScreen(
                     .padding(Paddings.xlarge, 40.dp).background(BackgroundColor),
             // todo: 수정
         ) {
-            if (shoppingDetailViewModel.detailContents.trade != null) { // 거래를 누군가와 하고 있음
-                if (shoppingDetailViewModel.detailContents.trade!!.isRequester) { // 그게 내가 건거야?
-                    if (shoppingDetailViewModel.detailContents.trade!!.status == TradeStatus.INPROGRESS.name) { // 완료 상태면
-                        CompleteSwapBottomButtonSection(
+            // 완료가 아님
+            if (shoppingDetailViewModel.detailContents.goodsTradeStatus != ProductTradeStatus.SOLDOUT.name) {
+                if (shoppingDetailViewModel.detailContents.trade != null) { // 거래를 누군가와 하고 있고
+                    if (shoppingDetailViewModel.detailContents.trade!!.isRequester) { // 그게 내가 건거야?
+                        if (shoppingDetailViewModel.detailContents.trade!!.status == TradeStatus.INPROGRESS.name) { // 수락 상태면
+                            CompleteSwapBottomButtonSection(
+                                navController,
+                                shoppingDetailViewModel,
+                                swapViewModel,
+                                chatViewModel = chatViewModel,
+                            )
+                        } else { // 완료 상태 아니면
+                            AfterSwapBottomButtonSection(
+                                shoppingDetailViewModel,
+                                swapViewModel,
+                                chatViewModel,
+                                navController,
+                                shoppingDetailViewModel,
+                            )
+                        }
+                    } else { // 아님 내가 받은 거야
+                        if (shoppingDetailViewModel.detailContents.trade!!.status == TradeStatus.INPROGRESS.name) { // 수락 상태면
+                            CompleteSwapBottomButtonSection(
+                                navController,
+                                shoppingDetailViewModel,
+                                swapViewModel,
+                                chatViewModel = chatViewModel,
+                            )
+                        } else { // 완료 상태 아니면
+                            ReceivedSwapBottomButtonSection(
+                                shoppingDetailViewModel,
+                                swapViewModel,
+                                chatViewModel,
+                                navController,
+                                shoppingDetailViewModel,
+                            )
+                        }
+                    }
+                } else { // 거래 안하고 있음
+                    if (userInfoViewModel.userInfo.value?.id != shoppingDetailViewModel.detailContents.user.userId) { // 내 물건이 아니면
+                        // 내 물건이 아니면 기본 버튼 보여줌
+                        BeforeSwapBottomButtonSection(
                             navController,
-                            shoppingDetailViewModel,
-                            swapViewModel,
+                            shoppingDetailViewModel = shoppingDetailViewModel,
                             chatViewModel = chatViewModel,
                         )
-                    } else { // 완료 상태 아니면
-                        AfterSwapBottomButtonSection(
-                            shoppingDetailViewModel,
-                            swapViewModel,
-                            chatViewModel,
-                            navController,
-                            shoppingDetailViewModel,
-                        )
                     }
-                } else { // 아님 내가 받은 거야
-                    if (shoppingDetailViewModel.detailContents.trade!!.status == TradeStatus.INPROGRESS.name) { // 완료 상태면
-                        CompleteSwapBottomButtonSection(
-                            navController,
-                            shoppingDetailViewModel,
-                            swapViewModel,
-                            chatViewModel = chatViewModel,
-                        )
-                    } else { // 완료 상태 아니면
-                        ReceivedSwapBottomButtonSection(
-                            shoppingDetailViewModel,
-                            swapViewModel,
-                            chatViewModel,
-                            navController,
-                            shoppingDetailViewModel,
-                        )
-                    }
-                }
-            } else { // 거래 안하고 있음
-                if (userInfoViewModel.userInfo.value?.id != shoppingDetailViewModel.detailContents.user.userId) { // 내 물건이 아니면
-                    // 내 물건이 아니면 기본 버튼 보여줌
-                    BeforeSwapBottomButtonSection(
-                        navController,
-                        shoppingDetailViewModel = shoppingDetailViewModel,
-                        chatViewModel = chatViewModel,
-                    )
                 }
             }
         }
