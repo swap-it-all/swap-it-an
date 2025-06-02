93d3f0eb88d06b039a912d4c50ccebedcad4a5bc
HJunng <guswjd1gh@gmail.com>
2025-06-02 17:25:57 +0900

    fix : 알림 팝업 아이콘 배경 색상 변경

--- app/src/main/java/com/swapit/oopswap/ui/alert/AlertTypeExtensions.kt
+++ app/src/main/java/com/swapit/oopswap/ui/alert/AlertTypeExtensions.kt
@@ -3,11 +3,11 @@ import com.swapit.oopswap.data.datasource.local.model.alert.AlertType
 
 fun AlertType.toIconResId(): Int =
     when (this) {
-        AlertType.CHAT -> R.drawable.ic_chat
-        AlertType.REQUESTED -> R.drawable.ic_shopping_bag
+        AlertType.CHAT -> R.drawable.ic_chat_bg_black
+        AlertType.REQUESTED,
         AlertType.ACCEPTED,
         AlertType.REJECTED,
         AlertType.COMPLETED,
-        -> R.drawable.ic_shopping_bag
-        AlertType.REVIEW -> R.drawable.ic_pencil
+        -> R.drawable.ic_shopping_bag_bg_black
+        AlertType.REVIEW -> R.drawable.ic_pencil_bg_black
     }
